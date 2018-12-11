c465710882e6caf223a306dbb2a6c44a77c31ef3
Konstantin <gibiskusus@gmail.com>
2018-12-11 17:01:59 +0100

    added collecting method

--- Reinforcement/ReinforcementControl.py
+++ Reinforcement/ReinforcementControl.py
@@ -8,6 +8,7 @@ import sys
 sys.path.append('../DataAcquisition')
 from MotorControl import api as MC
 import numpy as np
+import pandas as pd
 import CameraStream as CS
 import time
 
@@ -25,9 +26,14 @@ class ReinforcementControl():
         
         self.grabPos = [0]
         
-        self.step = [0,150,525]
+        self.step = [0,100,525]
 
-        self.state_space = np.array([[0,row,col] for row in np.arange(self.min_pos[1],self.max_pos[1]+self.step[1],self.step[1])\
+        self.state_space = np.array([[0,rob1,rob2,0,ball1,ball2] for rob1 in np.arange(self.min_pos[1],self.max_pos[1]+self.step[1],self.step[1])\
+                      for rob2 in np.arange(self.min_pos[2],self.max_pos[2]+self.step[2],self.step[2])\
+                      for ball1 in np.arange(self.min_pos[1],self.max_pos[1]+self.step[1],self.step[1])\
+                      for ball2 in np.arange(self.min_pos[2],self.max_pos[2]+self.step[2],self.step[2])])
+    
+        self.unit_state_space = np.array([[0,row,col] for row in np.arange(self.min_pos[1],self.max_pos[1]+self.step[1],self.step[1])\
                       for col in np.arange(self.min_pos[2],self.max_pos[2]+self.step[2],self.step[2])])
         self.action_space = [0,1,2,3] #['forward','backward','bend','unbend']
         #in the future probably we can add complex action like 'forward+bend'
@@ -41,6 +47,32 @@ class ReinforcementControl():
         else:
             self.curr_distance = self.min_dist * 10
         #self.reward_table = {act:[(1.0,[0,350])] for act in self.action_space}
+    
+    def collect_coordinates_of_states(self):
+        
+        # don't forget to write results in json file
+        # establish collecting 2-3 times and get mean values
+        count_collect = 3
+        pos = [0,0,0]
+        coord_space = np.zeros((count_collect,self.unit_state_space.shape[0],2))
+        
+        for num in range(count_collect):
+            for i,state in enumerate(self.unit_state_space):
+                pos = [int(state[0]),int(state[1]),int(state[2])]
+                print(pos)
+                self.mc.setPos(pos)
+                cam_data = self.cam_stream.get_data()
+                if(cam_data):
+                    coord_space[num,i] = cam_data[0]
+                else:
+                    print('Failed collecting coordinates on parameters (0,{}, {})'.format(state[1],state[2]))
+            print('Coordinate space. Collecting {}'.format(num))
+            print(coord_space[num])
+        med_coord_space = np.median(coord_space,axis=0)
+        df = pd.DataFrame(med_coord_space,columns=['x','y'])
+        df.to_csv('coord_motor_space.csv')
+        
+        return True
         
     
     def calculate_reward(self,curr_dist,new_dist):
@@ -130,6 +162,7 @@ class ReinforcementControl():
     
         
 #env = ReinforcementControl()
+#env.collect_coordinates_of_states()
 #env.mc.setPos([0,0,750])
 #print('plus step 1')
 #time.sleep(5)
